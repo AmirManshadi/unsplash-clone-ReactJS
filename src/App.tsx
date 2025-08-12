efa20263070f050c20b0ec39b5d07028eaa8ba68
AmirManshadi <amirmanshadi.code@gmail.com>
2025-08-12 16:19:28 +0330

    refactor: update import paths in App and main files for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
-import PhotoDetail from "./pages/photoDetail";
+import Home from "@/pages/home";
+import PhotoDetail from "@/pages/photoDetail";
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/photos/:id" element={<PhotoDetail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/photos/:id" element={<PhotoDetail />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
